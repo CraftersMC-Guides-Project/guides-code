d621426caa5b761e4579c723bc8bdc1a462b2092
Techon ;- <148909669+TechonVS@users.noreply.github.com>
2025-03-07 22:14:43 +0000

    Tweaks

--- js/script.js
+++ js/script.js
@@ -185,10 +185,10 @@ document.addEventListener("DOMContentLoaded", () => {
         });
       });
       elementsToStyle.statsBox.forEach(stat => (stat.style.backgroundColor = "rgba(0, 0, 0, 0.7)"));
-      elementsToStyle.statCard.forEach(card => (card.style.backgroundColor = "rgba(34, 34, 34, 0.4)"));
-      elementsToStyle.sellerDetails.forEach(detail => (detail.style.backgroundColor = "rgba(34, 34, 34, 0.4)"));
+      elementsToStyle.statCard.forEach(card => (card.style.backgroundColor = "rgba(0, 0, 0, 0.4)"));
+      elementsToStyle.sellerDetails.forEach(detail => (detail.style.backgroundColor = "rgba(0, 0, 0, 0.4)"));
       elementsToStyle.sellerProfile.forEach(profile => (profile.style.color = "white"));
-      elementsToStyle.sellerProfile.forEach(profile => (profile.style.backgroundColor = "rgba(68, 68, 68, 0.2)"));
+      elementsToStyle.sellerProfile.forEach(profile => (profile.style.backgroundColor = "rgba(34, 34, 34, 0.2)"));
 
       toggleButton.textContent = "light_mode";
     } else {
