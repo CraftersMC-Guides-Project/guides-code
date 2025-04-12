cfb8885051693bbe9beea11b6c9d7b71f829ec57
techonvs <techonedits@gmail.com>
2025-04-12 20:02:08 +0100

    prepping

---
+++ market/edit.js
@@ -0,0 +1,7 @@
+function saveInputs() {
+    let stringValue = document.getElementById('stringInput').value;
+    let numberValue = document.getElementById('numberInput').value;
+    
+    console.log('String value:', stringValue);
+    console.log('Number value:', numberValue);
+}
