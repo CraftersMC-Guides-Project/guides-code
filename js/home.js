5c097bda4204a493940ad442a40508286c9d50f8
Anshuman Mishra <gamerzp780@gmail.com>
2025-04-26 07:04:26 -0600

    Update home.js

--- js/home.js
+++ js/home.js
@@ -10,11 +10,13 @@ function clearSelection() {
 setTimeout(() => {
   const user = JSON.parse(localStorage.getItem("user"));
   const infoDiv = document.getElementById("sidebarUsername");
+  const infoDivName = document.getElementById("sidebarName");
 
   if (!user || !user.token) {
     window.location.href = "login.html"; // not logged in
   } else {
     infoDiv.innerHTML = `${user.email}`;
+    infoDivName.innerHTML = `${userSettings_`${user.email}`.username}`;
   }
 }, 5000); // 5000 milliseconds = 5 seconds
 
