5afe1e640f6bb6f9cadf75c6f918b19aa6f5dacd
Anshuman Mishra <gamerzp780@gmail.com>
2025-04-26 07:16:23 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -605,3 +605,22 @@ function toggleTheme(event) {
 //     });
 //   }
 // });
+
+setTimeout(() => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const userSettings = JSON.parse(localStorage.getItem(`userSettings_${user.email}`));
+  const infoDiv = document.getElementById("sidebarUsername");
+  const infoDivName = document.getElementById("sidebarName");
+
+  if (!user || !user.token) {
+    window.location.href = "login.html"; // not logged in
+  } else {
+    infoDiv.innerHTML = `${user.email}`;
+    infoDivName.innerHTML = `${userSettings.username}`;
+  }
+}, 5000); // 5000 milliseconds = 5 seconds
+
+function logout() {
+  localStorage.removeItem("user");
+  location.reload();
+}
