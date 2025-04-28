110d072ef5976d83f44d736ab5e8bef1e3b76bb1
Anshuman Mishra <gamerzp780@gmail.com>
2025-04-28 03:38:06 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -653,9 +653,9 @@ function checkLoginStatus() {
     var userLoggedIn = localStorage.getItem('discordUser') !== null;
     var settingsElement = document.getElementById('settings');
     if (userLoggedIn) {
-        settingsElement.innerHTML = '<span class="menu-icon material-icons" style="font-size: 38px; color: #ccc; onclick="logout()">logout</span>';
+        settingsElement.innerHTML = '<span class="menu-icon material-icons" style="font-size: 38px; color: #ccc;" onclick="logout()">logout</span>';
     } else {
-        settingsElement.innerHTML = '<span class="menu-icon material-icons" style="font-size: 38px; color: #ccc; onclick="login()">login</span>';
+        settingsElement.innerHTML = '<span class="menu-icon material-icons" style="font-size: 38px; color: #ccc;" onclick="login()">login</span>';
     }
 }
 
