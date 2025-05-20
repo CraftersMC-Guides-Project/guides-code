c11f897e5b6a77d03029614f477947443b8c3b8d
Anshuman Mishra <gamerzp780@gmail.com>
2025-05-20 07:16:28 -0600

    Update slayers_exp.js

--- js/slayers_exp.js
+++ js/slayers_exp.js
@@ -4,14 +4,14 @@ const collectionsData = [
     "image": "https://craftersmc.wiki.gg/images/8/8e/Revenant_Horror.png?b5e98d",
     "players": [
       { "rank": 1, "name": "SphinxWraith", "score": "40,000,000" },
-      { "rank": 2, "name": "DueBacon2007102", "score": "25,000,000" },
       { "rank": 3, "name": "HeGzoStRqFe", "score": "N/A" },
+      { "rank": 2, "name": "DueBacon2007102", "score": "25,000,000" },
       { "rank": 4, "name": "Itzomkar1232891", "score": "17,000,000" },
       { "rank": 5, "name": "StragglyBoat23", "score": "15,000,000+" },
       { "rank": 6, "name": "Kg_lcky_01", "score": "N/A" },
-      { "rank": 7, "name": "SoundClover8088", "score": "N/A" },
-      { "rank": 8, "name": "Firegod174", "score": "13,600,000" },
-      { "rank": 9, "name": "Shubh_gamer9497", "score": "12,400,000" },
+      { "rank": 7, "name": "Firegod174", "score": "N/A" },
+      { "rank": 8, "name": "DRONZAR_OP", "score": "N/A" },
+      { "rank": 9, "name": "SoundClover8088", "score": "N/A" },
       { "rank": 10, "name": "Arslaan9477", "score": "N/A" }
     ]
   },
