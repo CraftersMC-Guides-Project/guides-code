e03192c9b6dfba3bfd111203aa79d0813857a63e
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-05-25 22:52:10 +0530

    update js/slayers_exp.js

--- js/slayers_exp.js
+++ js/slayers_exp.js
@@ -3,15 +3,15 @@ const collectionsData = [
     "name": "Zombie Slayer",
     "image": "https://craftersmc.wiki.gg/images/8/8e/Revenant_Horror.png?b5e98d",
     "players": [
-      { "rank": 1, "name": "SphinxWraith", "score": "40,000,000" },
+      { "rank": 1, "name": "SphinxWraith", "score": "40M+" },
+      { "rank": 2, "name": "DueBacon2007102", "score": "25M+" },
       { "rank": 3, "name": "HeGzoStRqFe", "score": "N/A" },
-      { "rank": 2, "name": "DueBacon2007102", "score": "25,000,000" },
-      { "rank": 4, "name": "Itzomkar1232891", "score": "17,000,000" },
-      { "rank": 5, "name": "StragglyBoat23", "score": "15,000,000+" },
-      { "rank": 6, "name": "Kg_lcky_01", "score": "N/A" },
-      { "rank": 7, "name": "Firegod174", "score": "N/A" },
-      { "rank": 8, "name": "DRONZAR_OP", "score": "N/A" },
-      { "rank": 9, "name": "SoundClover8088", "score": "N/A" },
+      { "rank": 4, "name": "Itzomkar1232891", "score": "17M+" },
+      { "rank": 5, "name": "StragglyBoat23", "score": "15M+" },
+      { "rank": 6, "name": "SoundClover8088", "score": "N/A" },
+      { "rank": 7, "name": "Firegod174", "score": "16M+" },
+      { "rank": 8, "name": "Kg_lcky_01", "score": "N/A" },
+      { "rank": 9, "name": "Shubh_gamer9497", "score": "12.4M+" },
       { "rank": 10, "name": "Arslaan9477", "score": "N/A" }
     ]
   },
@@ -19,14 +19,14 @@ const collectionsData = [
     "name": "Wolf Slayer",
     "image": "https://craftersmc.wiki.gg/images/2/2e/Wolf_Spirit.png?e47d59",
     "players": [
-      { "rank": 1, "name": "SphinxWraith", "score": "6,000,000+" },
-      { "rank": 2, "name": "Herejefferson", "score": "6,000,000+" },
-      { "rank": 3, "name": "HeGzoStRqFe", "score": "3,000,000+" },
+      { "rank": 1, "name": "SphinxWraith", "score": "6M+" },
+      { "rank": 2, "name": "Herejefferson", "score": "6M+" },
+      { "rank": 3, "name": "HeGzoStRqFe", "score": "3M+" },
       { "rank": 4, "name": "Itzomkar1232891", "score": "N/A" },
-      { "rank": 5, "name": "Shakilbaby", "score": "2,500,000" },
+      { "rank": 5, "name": "Shakilbaby", "score": "2.5M+" },
       { "rank": 6, "name": "SoundClover8088", "score": "N/A" },
       { "rank": 7, "name": "Duebacon2007102", "score": "N/A" },
-      { "rank": 8, "name": "op_moraya5598", "score": "1,900,000" },
+      { "rank": 8, "name": "op_moraya5598", "score": "1.9M+" },
       { "rank": 9, "name": "uatitan580", "score": "N/A" },
       { "rank": 10, "name": "Unknown", "score": "N/A" }
     ]
