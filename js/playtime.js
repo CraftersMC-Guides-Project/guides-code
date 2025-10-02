08f458e3b13d6c1e82b8d02eb9efc025bdc70e52
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-02 13:11:05 +0530

    update js/playtime.js

---
+++ js/playtime.js
@@ -0,0 +1,12 @@
+const playtimeData = [
+    { rank: 1, name: "PlayerOne", playtime: "500h" },
+    { rank: 2, name: "PlayerTwo", playtime: "420h" },
+    { rank: 3, name: "PlayerThree", playtime: "390h" },
+    { rank: 4, name: "PlayerFour", playtime: "350h" },
+    { rank: 5, name: "PlayerFive", playtime: "300h" },
+    { rank: 6, name: "PlayerSix", playtime: "270h" },
+    { rank: 7, name: "PlayerSeven", playtime: "250h" },
+    { rank: 8, name: "PlayerEight", playtime: "220h" },
+    { rank: 9, name: "PlayerNine", playtime: "200h" },
+    { rank: 10, name: "PlayerTen", playtime: "180h" }
+];
