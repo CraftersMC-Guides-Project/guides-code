3c897f20696477435be08b618a420a2287405e1b
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-05 00:00:41 +0530

    Delete the file at 'js/edit.js'

--- js/edit.js
+++
@@ -1,7 +0,0 @@
-function saveInputs() {
-    let itemPrice = document.getElementById('priceInput').value;
-    let ID = document.getElementById('idInput').value;
-    
-    console.log('ID:', ID);
-    console.log('Price:', itemPrice);
-}
