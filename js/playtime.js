8124470337b58c8a8f092b386e8ca89863f7f494
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-07 00:20:32 +0530

    update js/playtime.js

--- js/playtime.js
+++ js/playtime.js
@@ -1,8 +1,8 @@
 const playtimeData = [
     { rank: 1, name: "gelacktic", playtime: "3841h" },
     { rank: 2, name: "Itzomkar1232891", playtime: "3001h" },
-    { rank: 3, name: "AK_GAMERR5398", playtime: "794.6h" },
-    { rank: 4, name: "Unknown", playtime: "N/A" },
+    { rank: 3, name: "ST4ANG3", playtime: "1576h" },
+    { rank: 4, name: "AK_GAMERR5398", playtime: "794.6h" },
     { rank: 5, name: "Unknown", playtime: "N/A" },
     { rank: 6, name: "Unknown", playtime: "N/A" },
     { rank: 7, name: "Unknown", playtime: "N/A" },
