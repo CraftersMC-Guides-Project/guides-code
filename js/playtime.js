ee7627aa7e5715953a98aaafbaf7b691c909fd83
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-07 22:03:33 +0530

    update js/playtime.js

--- js/playtime.js
+++ js/playtime.js
@@ -5,8 +5,8 @@ const playtimeData = [
     { rank: 4, name: "Itzomkar1232891", playtime: "3001h" },
     { rank: 5, name: "Kg_lcky_01", playtime: "2515.7h" },
     { rank: 6, name: "DRONZAR_OP", playtime: "2104h" },
-    { rank: 7, name: "ST4ANG3", playtime: "1576h" },
-    { rank: 8, name: "Mistry_Human", playtime: "1446.8h" },
-    { rank: 9, name: "Yashodon1233", playtime: "962.2h" },
-    { rank: 10, name: "MADERUCHIA07", playtime: "814.3h" }
+    { rank: 7, name: "Vishal2697144", playtime: "2026.8h" }
+    { rank: 8, name: "ST4ANG3", playtime: "1576h" },
+    { rank: 9, name: "Mistry_Human", playtime: "1446.8h" },
+    { rank: 10, name: "Yashodon1233", playtime: "962.2h" },
 ];
