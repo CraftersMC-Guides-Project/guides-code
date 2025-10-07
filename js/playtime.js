574cf90489a37606ec9b8988f7924be22e87ba00
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-07 09:47:52 +0530

    update js/playtime.js

--- js/playtime.js
+++ js/playtime.js
@@ -1,12 +1,12 @@
 const playtimeData = [
-    { rank: 1, name: "gelacktic", playtime: "3841h" },
-    { rank: 2, name: "Itzomkar1232891", playtime: "3001h" },
-    { rank: 3, name: "ST4ANG3", playtime: "1576h" },
-    { rank: 4, name: "Yashodon1233", playtime: "962.2h" },
-    { rank: 5, name: "AK_GAMERR5398", playtime: "794.6h" },
-    { rank: 6, name: "Unknown", playtime: "N/A" },
-    { rank: 7, name: "Unknown", playtime: "N/A" },
-    { rank: 8, name: "Unknown", playtime: "N/A" },
-    { rank: 9, name: "Unknown", playtime: "N/A" },
+    { rank: 1, name: "SphinxWraith", playtime: "6454.9h" },
+    { rank: 2, name: "gelacktic", playtime: "3841h" },
+    { rank: 3, name: "Itzomkar1232891", playtime: "3001h" },
+    { rank: 4, name: "DRONZAR_OP", playtime: "2104h" },
+    { rank: 5, name: "ST4ANG3", playtime: "1576h" },
+    { rank: 6, name: "Yashodon1233", playtime: "962.2h" },
+    { rank: 7, name: "MADERUCHIA07", playtime: "814.3h" },
+    { rank: 8, name: "AK_GAMERR5398", playtime: "794.6h" },
+    { rank: 9, name: "Ghosthunter4233", playtime: "736.9h" },
     { rank: 10, name: "Unknown", playtime: "N/A" }
 ];
