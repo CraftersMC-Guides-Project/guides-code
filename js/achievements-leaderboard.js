fe3b3f4e23e758b9c8f24a3dce7aaa4cf16badd3
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-09 18:26:49 +0530

    update js/achievements-leaderboard.js

---
+++ js/achievements-leaderboard.js
@@ -0,0 +1,12 @@
+const achievementsData = [
+    { rank: 1, name: "Unknown", achievements: "0" },
+    { rank: 2, name: "Unknown", achievements: "0" },
+    { rank: 3, name: "Unknown", achievements: "0" },
+    { rank: 4, name: "Unknown", achievements: "0" },
+    { rank: 5, name: "Unknown", achievements: "0" },
+    { rank: 6, name: "Unknown", achievements: "0" },
+    { rank: 7, name: "Unknown", achievements: "0" },
+    { rank: 8, name: "Unknown", achievements: "0" },
+    { rank: 9, name: "Unknown", achievements: "0" },
+    { rank: 10, name: "Unknown", achievements: "0" }
+];
