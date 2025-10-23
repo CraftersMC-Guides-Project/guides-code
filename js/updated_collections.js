395a5dd9854a61153f3c57674ae1362fe0922bec
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-23 23:13:33 +0530

    update js/updated_collections.js

--- js/updated_collections.js
+++ js/updated_collections.js
@@ -1873,13 +1873,13 @@ const collectionsData = {
                 },
                 {
                     "rank": 4,
-                    "name": "SeaborneClamp18",
-                    "score": "91.6M+"
+                    "name": "Watermark998517",
+                    "score": "92.1M+"
                 },
                 {
                     "rank": 5,
-                    "name": "Watermark998517",
-                    "score": "N/A"
+                    "name": "SeaborneClamp18",
+                    "score": "91.6M+"
                 },
                 {
                     "rank": 6,
