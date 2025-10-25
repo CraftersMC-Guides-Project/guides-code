b83ebf19b69bac8264b5beacda5efb805b517e08
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-25 23:59:35 +0530

    update js/avg-skill-leaderboard.js

--- js/avg-skill-leaderboard.js
+++ js/avg-skill-leaderboard.js
@@ -1,11 +1,11 @@
 const skillData = [
   { rank: 1, name: "SphinxWraith", level: "51.7" },
   { rank: 2, name: "Moerderfisch", level: "50.7" },
-  { rank: 3, name: "Perry3338", level: "39.5" },
-  { rank: 4, name: "TRIPLE_RRT", level: "28" },
-  { rank: 5, name: "Unknown", level: "N/A" },
-  { rank: 6, name: "Unknown", level: "N/A" },
-  { rank: 7, name: "Unknown", level: "N/A" },
+  { rank: 3, name: "DRONZAR_OP", level: "46.2" },
+  { rank: 4, name: "Rehan2038", level: "40.3" },
+  { rank: 5, name: "Perry3338", level: "39.5" },
+  { rank: 6, name: "Jatinsaini900", level: "35.7" },
+  { rank: 7, name: "The_Winchi", level: "35.2" },
   { rank: 8, name: "Unknown", level: "N/A" },
   { rank: 9, name: "Unknown", level: "N/A" },
   { rank: 10, name: "Unknown", level: "N/A" }
