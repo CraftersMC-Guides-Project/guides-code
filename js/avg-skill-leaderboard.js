f79b7737894ca6c978dfe2cf22ce3840bedcd75d
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-26 00:11:23 +0530

    update js/avg-skill-leaderboard.js

--- js/avg-skill-leaderboard.js
+++ js/avg-skill-leaderboard.js
@@ -1,12 +1,12 @@
 const skillData = [
   { rank: 1, name: "SphinxWraith", level: "51.7" },
   { rank: 2, name: "Moerderfisch", level: "50.7" },
-  { rank: 3, name: "DRONZAR_OP", level: "46.2" },
-  { rank: 4, name: "Rehan2038", level: "40.3" },
-  { rank: 5, name: "Perry3338", level: "39.5" },
-  { rank: 6, name: "Jatinsaini900", level: "35.7" },
-  { rank: 7, name: "The_Winchi", level: "35.2" },
-  { rank: 8, name: "Rayal_iscool", level: "34.7" },
-  { rank: 9, name: "AndroidPARAS", level: "34" },
-  { rank: 10, name: "Youngstunners7", level: "33.8" }
+  { rank: 3, name: "Itzomkar1232891", level: "47.2" },
+  { rank: 4, name: "DRONZAR_OP", level: "46.2" },
+  { rank: 5, name: "Rehan2038", level: "40.3" },
+  { rank: 6, name: "Perry3338", level: "39.5" },
+  { rank: 7, name: "Jatinsaini900", level: "35.7" },
+  { rank: 8, name: "The_Winchi", level: "35.2" },
+  { rank: 9, name: "Rayal_iscool", level: "34.7" },
+  { rank: 10, name: "AndroidPARAS", level: "34" }
 ];
