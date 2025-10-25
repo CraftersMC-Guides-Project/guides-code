dd9fcf769af3f28897fe1c2d291beb2ecdbf2f31
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-25 16:24:54 +0530

    update js/level-leaderboard.js

---
+++ js/level-leaderboard.js
@@ -0,0 +1,12 @@
+const levelsData = [
+  { rank: 1, name: "Unknown", level: "N/A" },
+  { rank: 2, name: "Unknown", level: "N/A" },
+  { rank: 3, name: "Unknown", level: "N/A" },
+  { rank: 4, name: "Unknown", level: "N/A" },
+  { rank: 5, name: "Unknown", level: "N/A" },
+  { rank: 6, name: "Unknown", level: "N/A" },
+  { rank: 7, name: "Unknown", level: "N/A" },
+  { rank: 8, name: "Unknown", level: "N/A" },
+  { rank: 9, name: "Unknown", level: "N/A" },
+  { rank: 10, name: "Unknown", level: "N/A" }
+];
