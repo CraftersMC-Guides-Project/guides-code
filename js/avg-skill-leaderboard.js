8a6c0b5587291d1759f9f4e3d3a56bb84e6e2b6c
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-26 18:31:35 +0530

    update js/avg-skill-leaderboard.js

--- js/avg-skill-leaderboard.js
+++ js/avg-skill-leaderboard.js
@@ -3,7 +3,7 @@ const skillData = [
   { rank: 2, name: "Moerderfisch", level: "50.7" },
   { rank: 3, name: "Itzomkar1232891", level: "47.2" },
   { rank: 4, name: "DRONZAR_OP", level: "46.2" },
-  { rank: 5, name: "Rehan2038", level: "40.3" },
+  { rank: 5, name: "Rehan2038", level: "41" },
   { rank: 6, name: "Perry3338", level: "39.5" },
   { rank: 7, name: "Kautilya3072", level: "36.2" },
   { rank: 8, name: "Jatinsaini900", level: "35.7" },
