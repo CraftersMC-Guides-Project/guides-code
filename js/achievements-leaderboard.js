1b79c401ec773d22d4669cd52183fec87a93a3ad
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-27 19:26:56 +0530

    update js/achievements-leaderboard.js

--- js/achievements-leaderboard.js
+++ js/achievements-leaderboard.js
@@ -1,9 +1,9 @@
 const achievementsData = [
     { rank: 1, name: "SphinxWraith", achievements: "70" },
     { rank: 2, name: "Kg_lcky_01", achievements: "64" },
+    { rank: 5, name: "MythicalPanda30", achievements: "60" },
     { rank: 3, name: "Perry3338", achievements: "51" },
     { rank: 4, name: "Kautilya3072", achievements: "46" },
-    { rank: 5, name: "Unknown", achievements: "0" },
     { rank: 6, name: "Unknown", achievements: "0" },
     { rank: 7, name: "Unknown", achievements: "0" },
     { rank: 8, name: "Unknown", achievements: "0" },
