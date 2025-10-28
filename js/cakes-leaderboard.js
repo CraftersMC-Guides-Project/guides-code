9b83ab4892793719b2ce7cf8a40e3a21613212c4
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-10-28 22:30:48 +0530

    update js/cakes-leaderboard.js

---
+++ js/cakes-leaderboard.js
@@ -0,0 +1,12 @@
+const cakeData = [
+  { rank: 1, name: "Unknown", cakes: "N/A" },
+  { rank: 2, name: "Unknown", cakes: "N/A" },
+  { rank: 3, name: "Unknown", cakes: "N/A" },
+  { rank: 4, name: "Unknown", cakes: "N/A" },
+  { rank: 5, name: "Unknown", cakes: "N/A" },
+  { rank: 6, name: "Unknown", cakes: "N/A" },
+  { rank: 7, name: "Unknown", cakes: "N/A" },
+  { rank: 8, name: "Unknown", cakes: "N/A" },
+  { rank: 9, name: "Unknown", cakes: "N/A" },
+  { rank: 10, name: "Unknown", cakes: "N/A" }
+];
