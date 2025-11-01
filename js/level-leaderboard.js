a8e138df9432ea4169784b03f58a13d8dd1ea44e
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-11-01 16:24:38 +0530

    update js/level-leaderboard.js

--- js/level-leaderboard.js
+++ js/level-leaderboard.js
@@ -1,11 +1,11 @@
 const levelsData = [
   { rank: 1, name: "SphinxWraith", level: "91" },
   { rank: 2, name: "Perry3338", level: "44" },
-  { rank: 3, name: "Dharmaveer31", level: "39" },
-  { rank: 4, name: "MythicalPanda30", level: "27" },
-  { rank: 5, name: "Kautilya3072", level: "16" },
-  { rank: 6, name: "SAAD_KING_07726", level: "15" },
-  { rank: 7, name: "Unknown", level: "N/A" },
+  { rank: 3, name: "Jeremy2075", level: "40" },
+  { rank: 4, name: "Dharmaveer31", level: "39" },
+  { rank: 5, name: "MythicalPanda30", level: "27" },
+  { rank: 6, name: "Kautilya3072", level: "16" },
+  { rank: 7, name: "SAAD_KING_07726", level: "15" },
   { rank: 8, name: "Unknown", level: "N/A" },
   { rank: 9, name: "Unknown", level: "N/A" },
   { rank: 10, name: "Unknown", level: "N/A" }
