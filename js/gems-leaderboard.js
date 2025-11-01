38c7eaa9c04e6be604c74b77dc9060b9a673dcb7
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-11-01 16:49:38 +0530

    update js/gems-leaderboard.js

--- js/gems-leaderboard.js
+++ js/gems-leaderboard.js
@@ -1,12 +1,12 @@
 const gemsData = [
-  { rank: 1, name: "Fallen1sec", gems: "400" },
-  { rank: 2, name: "Kautilya3072", gems: "25" },
-  { rank: 3, name: "Unknown", gems: "N/A" },
-  { rank: 4, name: "Unknown", gems: "N/A" },
-  { rank: 5, name: "Unknown", gems: "N/A" },
-  { rank: 6, name: "Unknown", gems: "N/A" },
-  { rank: 7, name: "Unknown", gems: "N/A" },
-  { rank: 8, name: "Unknown", gems: "N/A" },
-  { rank: 9, name: "Unknown", gems: "N/A" },
-  { rank: 10, name: "Unknown", gems: "N/A" }
+  { rank: 1, name: "Shakilbaby", gems: "52825" },
+  { rank: 2, name: "POSTALFERN73293", gems: "16426" },
+  { rank: 3, name: "KRIZMORRENO", gems: "12970" },
+  { rank: 4, name: "Mistry_Human", gems: "7625" },
+  { rank: 5, name: "Jaddu0192", gems: "7325" },
+  { rank: 6, name: "Chaosgem999yt", gems: "7128" },
+  { rank: 7, name: "Craniax6797", gems: "6290" },
+  { rank: 8, name: "Darkshadow52195", gems: "5779" },
+  { rank: 9, name: "Preenuu", gems: "3600" },
+  { rank: 10, name: "AhmedTheGuy6100", gems: "3509" }
 ];
