f8198c101dc3848e16b6ec9782193714f422b064
Jassi Singhania <139629332+TGJASSIYT@users.noreply.github.com>
2025-12-28 23:49:31 +0530

    update js/multi-lb-data.js

--- js/multi-lb-data.js
+++ js/multi-lb-data.js
@@ -49,8 +49,8 @@ const gemsData = [
   { rank: 6, name: "Jaddu0192", gems: "7325" },
   { rank: 7, name: "Chaosgem999yt", gems: "7128" },
   { rank: 8, name: "Craniax6797", gems: "6290" },
-  { rank: 9, name: "Darkshadow52195", gems: "5779" },
-  { rank: 10, name: "Preenuu", gems: "3600" }
+  { rank: 9, name: "Preenuu", gems: "3600" },
+  { rank: 10, name: "Unknown", gems: "N/A" }
 ];
 // Crafters Level 
 const levelsData = [
