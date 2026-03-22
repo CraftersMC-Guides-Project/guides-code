eaf8e8a0a2d54aec6b3ad18c5c5c524691d84e5b
PgNetwork01 <gamerzp780@gmail.com>
2026-03-22 13:51:17 +0530

    leaderboard api

--- functions/api/leaderboards.js
+++ functions/api/leaderboards.js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
   const origin = new URL(request.url).origin;
   const sources = {
-    multi: "../js/multi-lb-data.js",
+    multi: "/js/multi-lb-data.js",
     boss: "../js/boss-time-leaderboard.js",
     farming: "../js/farming-contest-leaderboard.js",
     collections: "../js/updated_collections.js",
