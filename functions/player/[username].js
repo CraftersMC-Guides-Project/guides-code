805d435803d5b9cce0b9620bde76f4502341a503
PgNetwork01 <gamerzp780@gmail.com>
2026-08-30 21:46:55 +0530

    maybe fix?

--- functions/player/[username].js
+++ functions/player/[username].js
@@ -1,14 +1,16 @@
 export async function onRequest({ params, env }) {
-    const username = params.username
+    const username = params.username;
+    const apiKey = env.CMC_API_KEY;
 
     const res = await fetch(
-        `https://api.craftersmc.net/v1/player/${username}`,
+        `https://api.craftersmc.net/v1/player/${encodeURIComponent(username)}`,
         {
             headers: {
-                'X-API-Key': env.CMC_API_KEY || env.cmc_api_key || env['cmc-api-key']
+                'X-API-Key': apiKey,
+                'Accept': 'application/json'
             }
         }
-    )
+    );
 
     return new Response(res.body, {
         status: res.status,
@@ -16,5 +18,5 @@ export async function onRequest({ params, env }) {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
         }
-    })
+    });
 }
--- functions/profile/[profileId].js
+++ functions/profile/[profileId].js
@@ -1,12 +1,15 @@
 export async function onRequest({ params, env }) {
+  const apiKey = env.CMC_API_KEY || env.cmc_api_key || env['cmc-api-key'] || env.CRAFTERS_API_KEY || env.crafters_api_key || env.CMC_KEY || env.API_KEY || env.CMC_API_KEY_BAZAAR || env.CMCG_BAZAAR_KEY || '';
+
   const res = await fetch(
-    `https://api.craftersmc.net/v1/skyblock/profile/${params.profileId}`,
+    `https://api.craftersmc.net/v1/skyblock/profile/${encodeURIComponent(params.profileId)}`,
     {
       headers: {
-        'X-API-Key': env.CMC_API_KEY || env.cmc_api_key || env['cmc-api-key']
+        'X-API-Key': apiKey,
+        'Accept': 'application/json'
       }
     }
-  )
+  );
 
   return new Response(res.body, {
     status: res.status,
@@ -14,5 +17,5 @@ export async function onRequest({ params, env }) {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*'
     }
-  })
+  });
 }
